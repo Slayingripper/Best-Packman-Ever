d7c655566889933a85bc223641e6abf6b727aeaa
Vasilis Ieropoulos <kirav96@gmail.com>
2018-12-14 03:19:09 +0000

    Gracefully closes the setup screen

--- BestPacmanEverV5/src/engine/Setupcontroll.java
+++ BestPacmanEverV5/src/engine/Setupcontroll.java
@@ -9,24 +9,31 @@ import javafx.fxml.FXMLLoader;
 import javafx.scene.Node;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
+import javafx.scene.control.Button;
 import javafx.scene.control.ColorPicker;
 import javafx.stage.Stage;
 
 public class Setupcontroll {
-    
+    @FXML Button	savebtn;
 	@FXML ColorPicker obstacle;
 	@FXML ColorPicker background;
 	
-	public void initialise() {
-	}
+	
 	
 	@FXML public void returntomain(ActionEvent event) throws IOException {
-		
+		/*Gets the values from the ColorPickers
+		 * Adds them to the their corresponding positions
+		 * hence if we pick Yellow for the background 
+		 * the value will be added in the controller class
+		 * and will change the background.
+		 */
+		closesstage();
 		colourpicker();
 		Parent start = FXMLLoader.load(getClass().getResource("set.fxml")) ;
 	    Scene startScene = new Scene(start);
 		Stage window = (Stage) ((Node)event.getSource()).getScene().getWindow();
 		window.setScene(startScene);
+		
 
 	}
 
@@ -35,7 +42,12 @@ public class Setupcontroll {
 		BarObstacle.obstacle = obstacle.getValue();
 		Controller.background = background.getValue();
 		
+		
 	}
 	
+	private void closesstage() {
+		Stage stage = (Stage) savebtn.getScene().getWindow();
+		stage.close();
+	}
 
 }
